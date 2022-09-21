3759ed24a58f7d6d72e87b435ecab86d147e28ae
Lewis <lewisdsykes@gmail.com>
2022-09-21 16:28:40 +0100

    "ISell items"

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -4,18 +4,11 @@ import behaviours.IPlay;
 import behaviours.ISell;
 
 public abstract class Instrument implements IPlay, ISell {
-
-
     protected double price;
     protected String material;
-
     protected String colour;
-
     protected InstrumentType instrumentType;
-
     protected String sound;
-
-
     public Instrument(double price, String material, String colour, InstrumentType instrumentType, String sound) {
         this.price = price;
         this.material = material;
@@ -23,24 +16,18 @@ public abstract class Instrument implements IPlay, ISell {
         this.instrumentType = instrumentType;
         this.sound = sound;
     }
-
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
     public String getMaterial() {
         return material;
     }
-
     public String getColour() {
         return colour;
     }
-
     public InstrumentType getInstrumentType() {
         return instrumentType;
     }
